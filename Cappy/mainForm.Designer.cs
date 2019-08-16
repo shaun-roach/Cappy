7439bcb680daa426275828aed2939a463825f264
Shaun Roach <shaun@roachfamily.co.uk>
2019-08-16 18:52:54 +0100

    Initial commit

---
+++ Cappy/mainForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Cappy
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.notifyCappy = new System.Windows.Forms.NotifyIcon(this.components);
+            this.cappyTimer = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyCappy
+            // 
+            this.notifyCappy.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyCappy.Icon")));
+            this.notifyCappy.Text = "Caps Lock";
+            this.notifyCappy.Visible = true;
+            this.notifyCappy.DoubleClick += new System.EventHandler(this.NotifyCappy_DoubleClick);
+            // 
+            // cappyTimer
+            // 
+            this.cappyTimer.Enabled = true;
+            this.cappyTimer.Interval = 500;
+            this.cappyTimer.Tick += new System.EventHandler(this.CappyTimer_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(282, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Little utility to show when CAPS LOCK is on.";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 40);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(279, 25);
+            this.panel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(303, 77);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Cappy v1.0.0";
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyCappy;
+        private System.Windows.Forms.Timer cappyTimer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Cappy/mainForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cappy
+{
+    public partial class MainForm : Form
+    {
+        private bool allowshowdisplay = false;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            this.CheckCapsLock();
+        }
+
+        private void CheckCapsLock()
+        {
+            if (Control.IsKeyLocked(Keys.CapsLock))
+            {
+                this.notifyCappy.Icon = Cappy.Properties.Resources.insert_text;
+                this.label1.Text = "Caps Lock is currently ON";
+            }
+            else
+            {
+                this.notifyCappy.Icon = Cappy.Properties.Resources.stop;
+                this.label1.Text = "Caps Lock is currently OFF";
+            }
+        }
+
+        protected override void SetVisibleCore(bool value)
+        {
+            base.SetVisibleCore(allowshowdisplay ? value : allowshowdisplay);
+        }
+
+        private void NotifyCappy_DoubleClick(object sender, EventArgs e)
+        {         
+            this.allowshowdisplay = true;
+            this.Visible = !this.Visible;         
+        }
+
+        private void CappyTimer_Tick(object sender, EventArgs e)
+        {
+            this.CheckCapsLock();
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+                this.WindowState = FormWindowState.Normal;
+            }
+        }
+    }
+}
